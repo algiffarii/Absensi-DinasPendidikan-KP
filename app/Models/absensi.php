5e81258024def55dded94a81a390df8ec381c1b8
algiffarii <110269841+algiffarii@users.noreply.github.com>
2023-01-16 02:50:11 +0800

    FIX: Model class name

--- app/Models/absensi.php
+++ app/Models/absensi.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class absensi extends Model
+class Absensi extends Model
 {
     use HasFactory;
     protected $table = 'absensi';
